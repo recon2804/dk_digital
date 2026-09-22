2f073de6c0cc6547536439caeba05f380853da6d
Kalny <DZCP2TJ@skoda.vwgroup.com>
2026-09-22 15:07:33 +0200

    Finished and checked all section HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Hamburger menu
 const menuButton = document.querySelector(".header__menu-button");
 const navigation = document.querySelector(".header__nav");
 const navigationLinks = document.querySelectorAll(".header__nav-link")
@@ -8,6 +9,7 @@ menuButton.addEventListener("click", () => {
     menuButton.setAttribute("aria-expanded", isOpen ? "Zavřít navigace" : "Otevřít navigaci");
 })
 
+//zavření hamburger menu po kliknutí
 navigationLinks.forEach((oneLink) => {
     oneLink.addEventListener("click", () => {
         navigation.classList.remove("is-open");
@@ -16,5 +18,30 @@ navigationLinks.forEach((oneLink) => {
 })
 });
 
+//Zachycení formuláře odeslat
+const contactForm = document.querySelector(".contact-form");
+const buttonInput = document.querySelector(".contact-form__button");
 
+contactForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
+    const formData = new FormData(contactForm);
+
+    buttonInput.textContent = "Odesílám...";
+    buttonInput.disabled = true;
+
+    try {
+        await new Promise((resolve) => {
+            setTimeout(resolve, 2000);
+        });
+
+        console.log("Poptávka byla úspěšně odeslána.");
+
+    } catch (error) {
+        console.error("Při odesílání nastala chyba.", error);
+
+    } finally {
+        buttonInput.disabled = false;
+        buttonInput.textContent = "Odeslat poptávku";
+    }
+});
